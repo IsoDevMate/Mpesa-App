f2eed8db73e8fa655b8bd3a35636837d1a721024
IsoDevMate <oumaoduor5827@gmail.com>
2023-05-04 23:52:42 +0300

    intergrating  daraja API

--- index.js
+++
@@ -1,3 +0,0 @@
-const express = require('express')
-const app =express()// define the routes and handle incoming HTTP requests.for an express app instance.
-const mpesa=require ('mpesa')
---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app =express()// define the routes/middleware and handle incoming HTTP requests.for an express app instance.
+const mpesa=require ('mpesa')
+
+//setting up daraja API  credentials
+const credentials={
+    client_key: 'ZsZB9g73A27tlAG9ysw93GmP4Ia9Ppwe',
+  client_secret: 'SpIkuMuBWQ9V4vsw',
+  initiator_name: 'your_initiator_name',
+  security_credential: 'your_security_credential',
+  certification_path: 'path/to/your/cert.pem',
+  environment: 'sandbox' // or 'production'
+}
+//creating new object with daraja API credentials
+
+const daraja= new mpesa(credentials)
+//creating routes handling incoming req from the frontend e.g endpoint for a initiating payment transaction
+app.post('/pay',(req,res)=>{
+    const amount=req.body.amount //paid amount
+    const phone=req.body.phone  //recipients phone number
+})
