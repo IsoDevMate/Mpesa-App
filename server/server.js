d5c44c5de05e586b3ac853a7174c87ca332afc0c
IsoDevMate <oumaoduor5827@gmail.com>
2023-05-11 18:37:34 +0300

    V.1.2.1

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app =express()// define the routes/middleware and handle incoming HTTP requests.for an express app instance.
 const mpesa=require ('mpesa')
+require("dotenv").config() //save secret keys
+const cors = require('cors') //allow API requests from different Domains
 
 //setting up daraja API  credentials
 const credentials={
@@ -35,3 +37,6 @@ const port = processs.env.PORT || 3000
 app.listen(port,()=>{
     console.log(`server listening  on ${port} `)
 })
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+app.use(cors())
